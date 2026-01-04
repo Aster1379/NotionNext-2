751b1a959ac8569cdf3a65802724b187a726b986
Aster1379 <3955280475@qq.com>
2026-01-04 22:00:49 +0800

    Refactor Coze.js to improve SDK loading logic

--- components/Coze.js
+++ components/Coze.js
@@ -9,15 +9,23 @@ import { useEffect } from 'react'
 export default function Coze() {
   const cozeSrc = siteConfig(
     'COZE_SRC_URL',
-    // 仅修改此处SDK版本号，升级到最新稳定版本
     'https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/1.2.0-beta.6/libs/cn/index.js'
   )
   const title = siteConfig('COZE_TITLE', 'NotionNext助手')
   const botId = siteConfig('COZE_BOT_ID')
+
   const loadCoze = async () => {
     await loadExternalResource(cozeSrc)
-    const CozeWebSDK = window?.CozeWebSDK if (CozeWebSDK) {
-      const cozeClient = new CozeWebSDK.WebChatClient({ config: { bot_id: botId }, componentProps: { title: title } })
+    const CozeWebSDK = window?.CozeWebSDK
+    if (CozeWebSDK) {
+      const cozeClient = new CozeWebSDK.WebChatClient({
+        config: {
+          bot_id: botId
+        },
+        componentProps: {
+          title: title
+        }
+      })
       console.log('coze', cozeClient)
     }
   }
@@ -28,6 +36,5 @@ export default function Coze() {
     }
     loadCoze()
   }, [])
-
-  return <> }
+  return <></>
 }
