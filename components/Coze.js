ed77bcad1b898e513f68048ab97263da91e217a0
Aster1379 <3955280475@qq.com>
2026-01-05 21:49:18 +0800

    Update COZE_SRC_URL to use latest version

--- components/Coze.js
+++ components/Coze.js
@@ -3,11 +3,10 @@ import { loadExternalResource } from '@/lib/utils'
 import { useEffect } from 'react'
 
 export default function Coze() {
-  const cozeSrc = siteConfig(
-    'COZE_SRC_URL',
-    'https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/1.2.0-beta.6/libs/cn/index.js'
+  const cozeSrc = siteConfig( 
+    'COZE_SRC_URL', 
+    'https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk@latest/libs/cn/index.js' 
   )
-  
   const title = siteConfig('COZE_TITLE', 'NotionNext助手')
   const botId = siteConfig('COZE_BOT_ID')
   const cozeToken = siteConfig('COZE_PAT_TOKEN', '')
@@ -56,5 +55,5 @@ export default function Coze() {
     loadCoze()
   }, [botId, cozeToken])
 
-  return <></>
+  return <> </>
 }
